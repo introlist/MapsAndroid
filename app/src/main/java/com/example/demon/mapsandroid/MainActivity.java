069d82a9abcdcabca467dc9dd0c8655ba210736a
introlist <gilflo01@hotmail.com>
2017-04-03 07:45:35 -0500

    v1.0
    uno

--- app/src/main/java/com/example/demon/mapsandroid/MainActivity.java
+++ app/src/main/java/com/example/demon/mapsandroid/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.demon.mapsandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
+public class MainActivity extends AppCompatActivity
+        implements OnMapReadyCallback {
+    private GoogleMap mapa;
+    private Button btnOpciones;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        btnOpciones = (Button)findViewById(R.id.btnOpciones);
+        btnOpciones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cambiarOpciones();
+            }
+        });
+    }
+    @Override
+    public void onMapReady(GoogleMap map) {
+        mapa = map;
+    }
+    private void cambiarOpciones()
+    {
+        mapa.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        mapa.getUiSettings().setZoomControlsEnabled(true);
     }
 }
