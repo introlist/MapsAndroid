256a808ab988c24b534381559a5e2a96303f389f
introlist <gilflo01@hotmail.com>
2017-04-03 07:54:41 -0500

    fin
    fin

--- app/src/main/java/com/example/demon/mapsandroid/MainActivity.java
+++ app/src/main/java/com/example/demon/mapsandroid/MainActivity.java
@@ -4,15 +4,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends AppCompatActivity
         implements OnMapReadyCallback {
     private GoogleMap mapa;
     private Button btnOpciones;
+    private Button btnMover;
+    private Button btnAnimar;
+    private Button btnPosicion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +35,27 @@ public class MainActivity extends AppCompatActivity
                 cambiarOpciones();
             }
         });
+        btnMover = (Button)findViewById(R.id.btnMover);
+        btnMover.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moverMerida();
+            }
+        });
+        btnAnimar = (Button)findViewById(R.id.btnAnimar);
+        btnAnimar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animarMerida();
+            }
+        });
+        btnPosicion = (Button)findViewById(R.id.btnPosicion);
+        btnPosicion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                obtenerPosicion();
+            }
+        });
     }
     @Override
     public void onMapReady(GoogleMap map) {
@@ -36,5 +65,36 @@ public class MainActivity extends AppCompatActivity
     {
         mapa.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
         mapa.getUiSettings().setZoomControlsEnabled(true);
+    }
+
+    private void moverMerida()
+    {
+        CameraUpdate camUpd1 =
+                CameraUpdateFactory
+                        .newLatLngZoom(new LatLng(20.9802, -89.7029), 10);
+        mapa.moveCamera(camUpd1);
+    }
+    private void animarMerida()
+    {
+        LatLng merida = new LatLng(20.9896055, -89.6167457);
+        CameraPosition camPos = new CameraPosition.Builder()
+                .target(merida) //Centramos el mapa en Merida
+                .zoom(19) //Establecemos el zoom en 19
+                .bearing(45) //Establecemos la orientación con el noreste arriba
+                .tilt(80) //Bajamos el punto de vista de la cámara 70 grados
+                .build();
+        CameraUpdate camUpd3 =
+                CameraUpdateFactory.newCameraPosition(camPos);
+        mapa.animateCamera(camUpd3);
+    }
+
+    private void obtenerPosicion()
+    {
+        CameraPosition camPos = mapa.getCameraPosition();
+        LatLng coordenadas = camPos.target;
+        double latitud = coordenadas.latitude;
+        double longitud = coordenadas.longitude;
+        Toast.makeText(this, "Latitud: " + latitud + " | Longitud: " + longitud,
+                Toast.LENGTH_SHORT).show();
     }
 }
